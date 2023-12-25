6e4b4054da493e6525cbb95e4ea3bea057abce66
“Ankit <22amtics124@gmail.com>
2023-12-25 18:23:40 +0530

    added cards

--- app/COMPONENTS/Carausel.tsx
+++ app/COMPONENTS/Carausel.tsx
@@ -6,17 +6,18 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade } from 'swiper/modules';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import Image from 'next/image';
+import Link from 'next/link';
 
 // Import Swiper styles
 import 'swiper/css';
 
-function Carausel(){
+function Carausel() {
   const progressCircle = useRef(null);
   const progressContent = useRef(null);
   const onAutoplayTimeLeft = (s: any, time: any, progress: any) => {
   };
   return (
-    <div className='mt-16'>
+    <div className='mt-16 p-3 sm:p-0 '>
       <Swiper
         centeredSlides={true}
         autoplay={{
@@ -29,20 +30,34 @@ function Carausel(){
         className="mySwiper"
       >
         <SwiperSlide>
-          <Image
-            src={"/carausal-image-1.jpg"}
-            alt="IMG1"
-            width={2000}
-            height={200}
-          />
+          <Link href='/'>
+            <Image
+              src={"/carausal-image-1.jpg"}
+              alt="IMG1"
+              width={2000}
+              height={200}
+            />
+          </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <Image
-            src={"/carausal-image-2.jpg"}
-            alt="IMG1"
-            width={2000}
-            height={200}
-          />
+          <Link href='/'>
+            <Image
+              src={"/carausal-image-3.jpg"}
+              alt="IMG1"
+              width={2000}
+              height={200}
+            />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link href='/'>
+            <Image
+              src={"/carausal-image-2.jpg"}
+              alt="IMG1"
+              width={2000}
+              height={200}
+            />
+          </Link>
         </SwiperSlide>
       </Swiper>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
 import Image from 'next/image'
 import Header from './COMPONENTS/Layouts/Header'
 import Carousel from './COMPONENTS/Carausel'
+import ActionAreaCard from './COMPONENTS/ActionAreaCard'
 
 export default function Home() {
   return (
     <main className="container">
-      <Header/>
+      <Header />
 
-      <Carousel/>
+      <Carousel />
+
+      {/* <div className="content w-full h-96 fixed"></div> */}
+
+        <div id='displayCards' className='px-4 sm:px-16 grid lg:grid-cols-3 lg-grid-cols-3 grid-cols-1 gap-10 mb-52'>
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+          <ActionAreaCard />
+      </div>
     </main>
   )
 }
